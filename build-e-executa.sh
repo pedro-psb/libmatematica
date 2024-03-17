b19480623fe89e27fc306447d45564f4a2013ebb
Pedro Brochado <pedropsb95@gmail.com>
2024-03-17 11:00:20 -0300

    Implementa modulo matematica.h
    O modulo fornece funções de seno, cosseno e tangente.
    Um exemplo de uso pode ser executado em src/main.c

--- build-e-executa.sh
+++ build-e-executa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p out
-rm out/*
-gcc -c src/libmatematica.c -o out/libmatematica.o
-gcc src/main.c out/libmatematica.o -o out/main -lm
+rm -rf out/*
+gcc -c src/matematica.c -o out/matematica.o
+gcc src/main.c out/matematica.o -o out/main -lm
 ./out/main
